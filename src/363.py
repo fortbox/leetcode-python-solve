cc33f1f959c3907c83889b944036d954441f1410
henryxiao <fortbox1988@outlook.com>
2020-01-01 15:11:30 +0800

    leetcode dp 363 python 别人的答案,为何用二分还是没明白

---
+++ src/363.py
@@ -0,0 +1,25 @@
+#  Copyright (c) 2020
+#  @Author: xiaoweixiang
+import bisect
+from typing import List
+
+
+class Solution:
+    def maxSumSubmatrix(self, matrix: List[List[int]], k: int) -> int:
+        row = len(matrix)
+        col = len(matrix[0])
+        res = float("-inf")
+        for left in range(col):
+            _sum = [0] * row
+            for right in range(left, col):
+                for j in range(row):
+                    _sum[j] += matrix[j][right]
+                arr = [0]
+                cur = 0
+                for tmp in _sum:
+                    cur += tmp
+                    loc = bisect.bisect_left(arr, cur - k)
+                    if loc < len(arr):
+                        res = max(cur - arr[loc], res)
+                    bisect.insort(arr, cur)
+        return res
